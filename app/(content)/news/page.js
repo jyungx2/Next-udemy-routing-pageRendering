de26a83cc9c03166c8f6976a302bd38af50d1e23
jiyoungnim <cloundyon31@gmail.com>
2025-02-09 13:21:41 +0900

    📝 160. Option 1: Client-side Data Fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,12 +1,50 @@
-import Link from "next/link";
-import { DUMMY_NEWS } from "@/dummy-news";
+"use client"; // useEffect hook is a hook that needs to run on the client-side.
+
 import NewsList from "@/components/news-list";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [news, setNews] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  // One way of loading data in a NextJS app(we could use in any React App).
+  // But not the best way of loading data when using NextJS.
+  useEffect(() => {
+    async function fetchNews() {
+      const res = await fetch("http://localhost:8080/news");
+
+      if (!res.ok) {
+        setError("Failed to fetch news.");
+        setIsLoading(false);
+      }
+
+      const news = await res.json();
+      setIsLoading(false);
+      setNews(news);
+    }
+
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
